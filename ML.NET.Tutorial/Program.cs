62c06031871c419bdebae0277a6646a6a9c67032
Mark <k00213234@gmail.com>
2020-05-28 04:24:10 +0100

    Add project files.

---
+++ ML.NET.Tutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ML.NET.Tutorial
+{
+	using System;
+	/// <summary>
+	/// ML.NET Tutorial Program
+	/// https://dotnet.microsoft.com/learn/ml-dotnet/get-started-tutorial/intro
+	/// </summary>
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
